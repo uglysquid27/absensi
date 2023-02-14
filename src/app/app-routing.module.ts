5b4a81f67c459e5e328924c25eac25af804c98f4
Ilham Ibrahim <ii.ilhamibrahim@gmail.com>
2023-02-14 16:35:50 +0700

    router fixing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
+import { PesertaComponent } from './peserta/peserta.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // import { AppComponent } from './app.component';
 
-import { ChartComponent } from './contents/chart/chart.component';
-import { PatientComponent } from './contents/patient/patient.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'charts', component: ChartComponent },
-  { path: 'patients', component: PatientComponent },
+  { path: 'peserta', component: PesertaComponent}
 ];
 
 @NgModule({
