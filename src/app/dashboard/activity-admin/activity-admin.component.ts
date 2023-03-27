91a2e6c0df3efc942debbe9612df88447bed8a3f
satriyarp <rifkisatriya@gmail.com>
2023-03-27 09:06:32 +0700

    Add content Activity Admin

--- src/app/dashboard/activity-admin/activity-admin.component.ts
+++ src/app/dashboard/activity-admin/activity-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AttendanceService } from 'src/app/service/attendance.service';
 
 @Component({
   selector: 'app-activity-admin',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./activity-admin.component.css']
 })
 export class ActivityAdminComponent {
-
+  usersData: any;
+  constructor(private apiService: AttendanceService) {
+    apiService.getProfile().subscribe((data) => {
+      this.usersData = data;
+      console.log(this.usersData.data);
+    });
+  }
 }
