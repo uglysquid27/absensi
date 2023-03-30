8738f195686e826eb723467f3ad6d0c7b7d2c980
satriyarp <rifkisatriya@gmail.com>
2023-03-30 08:25:10 +0700

    Fix Sidebar

--- src/app/layouts/sidebar/sidebar.component.ts
+++ src/app/layouts/sidebar/sidebar.component.ts
@@ -8,16 +8,21 @@ import { Router } from '@angular/router';
 })
 export class SidebarComponent {
   boolDropdown: Boolean = false;
+  hideElement: Boolean = true;
   constructor(public router: Router) {
     // console.log(this.router.url)
   }
   onMouseEnter() {
+    this.hideElement = false;
+    console.log(this.hideElement);
     
   }
   onMouseOut() {
     console.log('out');
-    
     this.boolDropdown = false;
+    this.hideElement = true;
+    console.log(this.hideElement);
+    
   }
 
   dropdown() {
